943de9a5d9f994fb333d5b42195b86c3778fc5cd
xalexey <saleksey67@gmail.com>
2016-07-22 14:04:47 +0300

    Ошибка с CURL пропала

--- myspider/spider.py
+++ myspider/spider.py
@@ -1,8 +1,7 @@
 from grab import Grab
 
 g = Grab(log_file='out.html')
-g.setup(proxy='localhost:6588', proxy_type='http')
-
-g.go('http://localhost/blog/')
+g.setup(proxy='127.0.0.1:6588', proxy_type='http')
+g.go('http://localhost')
 
 
